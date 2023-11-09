92fe6fc78fbbbefff84a12c5598ebdeca1257cc7
Robert Forynski <forynski@gmail.com>
2023-11-09 19:59:56 +0000

    Real-time data capture - position

--- iOS-IoT-App/ViewController.swift
+++ iOS-IoT-App/ViewController.swift
@@ -6,9 +6,9 @@ import DGCharts
 class ViewController: UIViewController {
     @IBOutlet var chartView: LineChartView!
     
-    // Comment out the motion manager and dataEntries for real-time data capture
-    // let motionManager = CMMotionManager()
-    // var dataEntries: [ChartDataEntry] = []
+    // Motion manager and dataEntries for real-time data capture
+    let motionManager = CMMotionManager()
+    var dataEntries: [ChartDataEntry] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         chartView.chartDescription.text = "Sample Data"  // Chart description
 
         // Load sample data into the chart
+        /*
             let sampleData: [ChartDataEntry] = [
                 ChartDataEntry(x: 0.0, y: 0.5),
                 ChartDataEntry(x: 1.0, y: 0.7),
@@ -29,9 +30,10 @@ class ViewController: UIViewController {
             let dataSet = LineChartDataSet(entries: sampleData, label: "Sample Data")
             let data = LineChartData(dataSet: dataSet)
             chartView.data = data
+         */
 
-        // Comment out the real-time data capture
-        /*
+        // Real-time data capture
+        
         if motionManager.isAccelerometerAvailable {
             motionManager.accelerometerUpdateInterval = 0.1  // Update interval in seconds
 
@@ -48,12 +50,10 @@ class ViewController: UIViewController {
         } else {
             print("Accelerometer not available.")
         }
-        */
+        
     }
 
-    // Comment out the addEntry function for real-time data capture
-    /*
-    // Function to add a data entry to the chart
+    // Function to add real-time data capture to the chart
     func addEntry(x: Double) {
         dataEntries.append(ChartDataEntry(x: Double(dataEntries.count), y: x))
         let dataSet = LineChartDataSet(entries: dataEntries, label: "X-Axis Data")
@@ -61,14 +61,12 @@ class ViewController: UIViewController {
         chartView.data = data
         chartView.notifyDataSetChanged()
     }
-    */
+    
 
-    // Comment out the viewWillDisappear function for real-time data capture
-    /*
     // Be sure to stop the accelerometer updates when they're no longer needed
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         motionManager.stopAccelerometerUpdates()
     }
-    */
+    
 }
